00ba60807e1b4577c32b62f77201fa5a1b0f464c
sakshi_1331 <sakshi.1331agarwal@gmail.com>
2019-03-23 15:50:17 +0530

    bootstrap and train

--- com.py
+++ com.py
@@ -1,4 +1,7 @@
 import pickle
+from sklearn.svm import SVC
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.utils import resample
 
 list_file_p =   ["loan_p","netbank_p","imd_p","ppf_p","travelcard_p"]
 list_file_ans =   ["loan_ans","netbank_ans","imd_ans","ppf_ans","travelcard_ans"]
@@ -27,6 +30,58 @@ file = open('database','wb')
 pickle.dump(data,file)
 file.close()
 
+unique = []
+
+for item in data:  ## creating vocabulary 
+    for word in item[0]:
+        if(word not in unique):
+            unique.append(word)
+
+            
+finalvect = []
+
+for item in data:
+    d = {}
+    for word in item[0]:
+        keys = d.keys()
+        if(word not in keys):
+            d[word] = 1
+        else:
+            val = d[word]
+            d[word] = val + 1
+    vect = [] ## bag of words vector for this question
+    for w in unique:
+        if(w not in d.keys()):
+            vect.append(0)
+        else:
+            vect.append(d[w])
+    finalvect.append(vect)
+
+
+
+
+indexlist = [40,44,18,15,16] ## indexlist contains number of records of each class
+sumindex = 0
+for i in range(5):
+    ansdata = [] ## all records of a particular class , input to bootstraping
+    for j in range(indexlist[i]):
+        l = []
+        l.append(finalvect[j + sumindex])
+        l.append(data[j + sumindex][2])    
+           
+        ansdata.append(l)
+    sumindex += indexlist[i]         
+    bootdata =  resample(ansdata, replace=True, n_samples= 75, random_state=1)   
+    SVC_classifier = SklearnClassifier(SVC())
+    SVC_classifier.train(bootdata)    
+        
+                 
+
+
+
+
+
+    
 
 
 
